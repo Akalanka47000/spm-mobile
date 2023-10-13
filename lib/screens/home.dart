723f4444dfe1c32ac4c02bf8409d085b802db55e
Akalanka <akalankaperera128@gmail.com>
2023-10-13 10:36:45 +0530

    feat: added tel launch

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,9 +1,9 @@
 import 'dart:convert';
-import 'dart:io' show Platform;
 import 'package:flutter/material.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 import 'package:spm_mobile/services/api.service.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -58,118 +58,124 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: scaffoldKey,
-      appBar: AppBar(
-        title: const Text(
-          'Scan Product',
-          style: TextStyle(color: Colors.black, fontSize: 22, fontWeight: FontWeight.w600),
-        ),
-        backgroundColor: Colors.white,
-        centerTitle: true,
-      ),
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            flex: 10,
-            child: Stack(
-              children: [
-                QRView(
-                  key: qrKey,
-                  onQRViewCreated: _onQRViewCreated,
-                ),
-                if (loading)
-                  Container(
-                    alignment: Alignment.center,
-                    width: double.infinity,
-                    height: double.infinity,
-                    color: Colors.black.withOpacity(0.4),
-                    child: const CircularProgressIndicator(
-                      color: Colors.white,
-                    ),
-                  )
-              ],
-            ),
+    return GestureDetector(
+      onDoubleTap: () async {
+        await flutterTts.speak("Calling support");
+        launchUrl(Uri.parse("tel:+943410607"), mode: LaunchMode.platformDefault);
+      },
+      child: Scaffold(
+        key: scaffoldKey,
+        appBar: AppBar(
+          title: const Text(
+            'Scan Product',
+            style: TextStyle(color: Colors.black, fontSize: 22, fontWeight: FontWeight.w600),
           ),
-          const Expanded(
-            flex: 1,
-            child: Center(
-              child: Text(
-                'Scan a product tag',
-                style: TextStyle(color: Colors.black, fontSize: 24, fontWeight: FontWeight.w500),
+          backgroundColor: Colors.white,
+          centerTitle: true,
+        ),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 10,
+              child: Stack(
+                children: [
+                  QRView(
+                    key: qrKey,
+                    onQRViewCreated: _onQRViewCreated,
+                  ),
+                  if (loading)
+                    Container(
+                      alignment: Alignment.center,
+                      width: double.infinity,
+                      height: double.infinity,
+                      color: Colors.black.withOpacity(0.4),
+                      child: const CircularProgressIndicator(
+                        color: Colors.white,
+                      ),
+                    )
+                ],
               ),
             ),
-          )
-        ],
-      ),
-      drawer: SizedBox(
-        width: MediaQuery.of(context).size.width * 0.9,
-        child: Drawer(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25),
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(
-                    height: 80,
-                  ),
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: Image.memory(
-                      const Base64Decoder().convert(product != null ? product['image'].toString().split(",")[1] : ''),
+            const Expanded(
+              flex: 1,
+              child: Center(
+                child: Text(
+                  'Scan a product tag',
+                  style: TextStyle(color: Colors.black, fontSize: 24, fontWeight: FontWeight.w500),
+                ),
+              ),
+            )
+          ],
+        ),
+        drawer: SizedBox(
+          width: MediaQuery.of(context).size.width * 0.9,
+          child: Drawer(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(
+                      height: 80,
                     ),
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  Text(
-                    'Product Name - ${product != null ? product['name'] : ''}',
-                    style: const TextStyle(color: Colors.black, fontSize: 22, fontWeight: FontWeight.w600),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    'Price - LKR ${product != null ? product['selling_price'] : ''}',
-                    style: const TextStyle(color: Colors.black, fontSize: 21, fontWeight: FontWeight.w600),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    'Category - ${product != null ? product['type'] : ''}',
-                    style: const TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.w600),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    'Unit - ${product != null ? product['measurement_unit'] : ''}',
-                    style: const TextStyle(color: Colors.black, fontSize: 18, fontWeight: FontWeight.w500),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    'Description - ${product != null ? product['description'] : ''}',
-                    style: const TextStyle(color: Colors.black, fontSize: 18, fontWeight: FontWeight.w500),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    'Seller name - ${product != null ? product['seller']['name'] : ''}',
-                    style: const TextStyle(color: Colors.black, fontSize: 18, fontWeight: FontWeight.w500),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    'Seller mobile - ${product != null ? product['seller']['mobile'] : ''}',
-                    style: const TextStyle(color: Colors.black, fontSize: 18, fontWeight: FontWeight.w500),
-                  ),
-                ],
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.memory(
+                        const Base64Decoder().convert(product != null ? product['image'].toString().split(",")[1] : ''),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    Text(
+                      'Product Name - ${product != null ? product['name'] : ''}',
+                      style: const TextStyle(color: Colors.black, fontSize: 22, fontWeight: FontWeight.w600),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      'Price - LKR ${product != null ? product['selling_price'] : ''}',
+                      style: const TextStyle(color: Colors.black, fontSize: 21, fontWeight: FontWeight.w600),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      'Category - ${product != null ? product['type'] : ''}',
+                      style: const TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.w600),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      'Unit - ${product != null ? product['measurement_unit'] : ''}',
+                      style: const TextStyle(color: Colors.black, fontSize: 18, fontWeight: FontWeight.w500),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      'Description - ${product != null ? product['description'] : ''}',
+                      style: const TextStyle(color: Colors.black, fontSize: 18, fontWeight: FontWeight.w500),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      'Seller name - ${product != null ? product['seller']['name'] : ''}',
+                      style: const TextStyle(color: Colors.black, fontSize: 18, fontWeight: FontWeight.w500),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      'Seller mobile - ${product != null ? product['seller']['mobile'] : ''}',
+                      style: const TextStyle(color: Colors.black, fontSize: 18, fontWeight: FontWeight.w500),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
